b2657bb73a6fb36071b5acfec68953ad8c9987f5
Zorahidaconh <zorahida86@gmail.com>
2023-03-10 22:47:38 +0100

    ejerciciosJS

---
+++ S1/1.variables/exercise-7.js
@@ -0,0 +1,9 @@
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+var globalBasePrice = 25000;
+console.log(globalBasePrice);
+
+var finalPrice(car1, car2) = basePrice(car1)(car2) + globalPrice(car1)(car2);
+console.log(finalPrice);
